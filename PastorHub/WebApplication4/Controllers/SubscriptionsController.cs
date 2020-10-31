29edf574753df68fd6b1945c2d65a1fe690026ac
Constantinus-C-E <aquaitstep@gmail.com>
2020-10-31 16:15:52 +0300

    2

---
+++ PastorHub/WebApplication4/Controllers/SubscriptionsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class SubscriptionsController : Controller
+    {
+        // GET: Subscriptions
+        public ActionResult Index()
+        {
+            ApplicationUser user = new ApplicationUser();
+            ApplicationDbContext context = new ApplicationDbContext();
+            string userId = User.Identity.GetUserId();
+            user = context.Users.Where(i => i.Id == userId).FirstOrDefault();
+            return View(user);
+        }
+    }
+}
